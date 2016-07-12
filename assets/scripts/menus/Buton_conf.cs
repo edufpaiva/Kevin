cb862314f06e64043ed7b00b06b9698826349f5c
rst2trindade <rst2trindade@gmail.com>
2016-07-12 18:02:54 -0400

    [Panel menu] foi adicionado um panel no menu do jogo;

---
+++ assets/scripts/menus/Buton_conf.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Buton_conf : MonoBehaviour
+{
+    private bool MenuOpctionActive = true;
+    public GameObject panelmenu;
+    public GameObject [] matrix; 
+
+
+    // Use this for initialization
+    void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	public void Panel_active()
+    {
+        MenuOpctionActive = !MenuOpctionActive;
+
+        if (MenuOpctionActive)
+        {
+            for (int i = 0; i < matrix.Length; i++)
+            {
+              matrix[i].SetActive(false); 
+            }
+            panelmenu.SetActive(true);
+        }
+        else
+        {
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                matrix[i].SetActive(true);
+            }
+            panelmenu.SetActive(false);
+        }
+	
+	}
+}
