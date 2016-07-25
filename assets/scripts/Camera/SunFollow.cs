6385b8a9e357a6ade4c9e643d1ab2963b58ee994
Eduardo Jorge Firmino de Paiva <edufpaiva33@gmail.com>
2016-07-25 01:37:50 -0400

    [SUNFOLLOW]

--- assets/scripts/Camera/SunFollow.cs
+++ assets/scripts/Camera/SunFollow.cs
@@ -1,19 +1,20 @@
 using System;
 using UnityEngine;
 
-    public class Camera2DFollow : MonoBehaviour
+    public class SunFollow : MonoBehaviour
     {
         public Transform target;
         public float damping = 1;
         public float lookAheadFactor = 3;
         public float lookAheadReturnSpeed = 0.5f;
         public float lookAheadMoveThreshold = 0.1f;
+        public float altura = 3;
 
         private float m_OffsetZ;
         private Vector3 m_LastTargetPosition;
         private Vector3 m_CurrentVelocity;
         private Vector3 m_LookAheadPos;
-
+        
         // Use this for initialization
         private void Start()
         {
@@ -42,9 +43,9 @@ using UnityEngine;
 
             Vector3 aheadTargetPos = target.position + m_LookAheadPos + Vector3.forward*m_OffsetZ;
             Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref m_CurrentVelocity, damping);
-
+        newPos = Vector3.Lerp(newPos, new Vector3(transform.position.x, target.transform.position.y + altura, 0), Time.deltaTime /2);
             transform.position = newPos;
-
+            
             m_LastTargetPosition = target.position;
         }
     }
