73e64bef231e7e5c1e280f93fdf03d38e5ba3616
Eduardo Jorge Firmino de Paiva <edufpaiva33@gmail.com>
2016-07-27 01:26:48 -0400

    [BUGS]  - Corrigido musica continuar tocando ao voltar ao menu pelo escape  - Corrigido a não aparição do menu ao morrer no final da fase 1

--- assets/scripts/GameManager/FinalDaFase.cs
+++ assets/scripts/GameManager/FinalDaFase.cs
@@ -18,22 +18,39 @@ public class FinalDaFase : MonoBehaviour {
         GameManager.Instance.SetFinalDaFase(numeroDaFase, false);
 
     }
+    void Update() {
+        if (GameManager.Instance.GetPlayerMorto())
+        {
+            Destroy(gameObject);
+        }
+    }
 
 
  
 
     void OnCollisionEnter2D(Collision2D col) {
-        if (col.gameObject.tag == "Player") {
-            if (!jogadorEsteveAqui && !GameManager.Instance.GetPlayerMorto()) {
-                
-                GameManager.Instance.SetFinalDaFase(numeroDaFase, true);
-                mCanvas[0].SetActive(false);
-                mCanvas[1].SetActive(true);
+        if (GameManager.Instance.GetPlayerMorto())
+        {
 
+        }
+        else {
+            if (col.gameObject.tag == "Player")
+            {
+                if (!jogadorEsteveAqui && !GameManager.Instance.GetPlayerMorto())
+                {
+
+                    GameManager.Instance.SetFinalDaFase(numeroDaFase, true);
+                    mCanvas[0].SetActive(false);
+                    mCanvas[1].SetActive(true);
+
+
+                }
 
             }
 
         }
+
+        
 
 
 
---
+++ assets/scripts/GameManager/MenuSound.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuSound : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameManager.Instance.GetComponent<SceneManager>().SetMenuMusic();
+	}
+	
+
+}
