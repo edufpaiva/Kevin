cf4daf1ce63fc51628cd988c0536ff6cff76daf4
Eduardo Jorge Firmino de Paiva <edufpaiva33@gmail.com>
2016-07-26 22:17:17 -0400

    [ESCAPE]  - adicionado retorno de tela com escape  - adicionado saida de jogo com escape

---
+++ assets/scripts/Escape/EscapeComPergunta.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EscapeComPergunta : MonoBehaviour {
+    private bool active = false;
+    public GameObject tela;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SetActive();
+        }
+	}
+
+    public void Exit() {
+        Application.Quit();
+
+    }
+
+    public void SetCena(string nomeDaCena) {
+
+        GameManager.Instance.GetComponent<SceneManager>().CarregarCena(nomeDaCena);
+    }
+    
+    public void SetActive(){
+        active = !active;
+        tela.SetActive(active);
+
+    }
+}
