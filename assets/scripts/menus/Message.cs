33f2f86dc67ca955d5518ef850deb2e58177b9c6
Eduardo Jorge F. de Paiva <edufpaiva33@gmail.com>
2016-07-28 00:48:44 -0300

    [GERAL]  - BG cena de seleçao adicionado  - Mensagem na 4 fase adicionada

---
+++ assets/scripts/menus/Message.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Message : MonoBehaviour {
+    public GameObject message;
+    private bool active = false;
+
+
+    public void SetMessageActive() {
+        active = !active;
+        message.SetActive(active);
+        
+    }
+
+
+    
+}
