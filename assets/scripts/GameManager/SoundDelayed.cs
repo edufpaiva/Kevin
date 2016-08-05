71cbd85149c412164c060baab17f4ce597478b0a
Eduardo Jorge Firmino de Paiva <edufpaiva33@gmail.com>
2016-08-05 15:57:17 -0400

    [...]

---
+++ assets/scripts/GameManager/SoundDelayed.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundDelayed : MonoBehaviour {
+    public AudioSource som;
+    public float timer;
+
+	
+	void Start () {
+        som.PlayDelayed(timer);
+	}
+	
+
+}
